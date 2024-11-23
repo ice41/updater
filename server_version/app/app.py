3094d4a48de6da083d89d3318e37aa20e0c080c1
ice41 <edu.ice7@gmail.com>
2024-11-23 22:21:06 +0000

    Update app.py

--- server_version/app/app.py
+++ server_version/app/app.py
@@ -82,7 +82,7 @@ class UpdaterApp(MDApp):
             logo_layout = AnchorLayout(anchor_x='center', anchor_y='top', size_hint=(1, None), height=200)
            # logo_card = MDCard(size_hint=(None, None), size=(150, 200), elevation=8)
             logo_card.add_widget(KivyImage(source=logo_path))
-            logo_layout.add_widget(logo_card)
+            #logo_layout.add_widget(logo_card)
             main_layout.add_widget(logo_layout)
 
         # Versões
