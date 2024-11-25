7ed25efa31365ca274bef2e198856de5e8d14756
ice41 <edu.ice7@gmail.com>
2024-11-25 22:06:41 +0000

    Update app.py

--- server_version/app/app.py
+++ server_version/app/app.py
@@ -15,6 +15,7 @@ from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.card import MDCard
 from kivy.config import Config
 from kivymd.uix.image import Image as KivyImage
+from kivy.uix.image import Image
 
 # URLs para os arquivos remotos
 NEWS_URL = "https://raw.githubusercontent.com/ice41/updater/refs/heads/main/server_version/app/news.py"
