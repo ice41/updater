c144423947dfda0bb8fd592a624b4a4fc9e597ee
ice41 <edu.ice7@gmail.com>
2024-11-27 20:25:37 +0000

    Update app.py

--- server_version/app/app.py
+++ server_version/app/app.py
@@ -5,7 +5,6 @@ import requests
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.label import MDLabel
-from kivymd.uix.button import MDRaisedButton, MDRectangleFlatButton
 from kivymd.uix.progressbar import MDProgressBar
 from kivy.core.window import Window
 from kivy.uix.anchorlayout import AnchorLayout
